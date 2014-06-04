856bfa3c4232284e21fcdd7f658a47a5d2294938
Neklussy <lusalane@hotmail.com>
2014-06-04 13:24:06 +0200

    version inicial

---
+++ EjemploPruebasLaura/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EjemploPruebasLaura.Global" Language="C#" %>
---
+++ PruebasUnitarias/UsuarioTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EjemploPruebasLaura.Model;
+using System.Security.Cryptography;
+
+namespace PruebasUnitarias
+{
+    /// <summary>
+    /// Summary description for UsuarioTest
+    /// </summary>
+    [TestClass]
+    public class UsuarioTest
+    {
+        public void ToStringTest() {
+            Usuario u = new Usuario();
+
+            Assert.AreEqual(u.ToString(), "admin;aaa111...");
+        }
+
+        [TestMethod]
+        public void UsuarioConstructorTest() {
+            Usuario u = new Usuario();
+            Assert.IsNotNull(u);
+            Assert.AreEqual(u.username, "admin");
+            Assert.AreEqual(u.password, "aaa111...");
+            //hash 15827d1b83c6be8de449fa4535737d48
+        }
+
+
+        [TestMethod]
+        //md5 no es, hay que poner el nombre de la funcion que da la excepción
+        [ExpectedException(typeof(MD5))]
+        public void getMD5Test() {
+            Usuario u = new Usuario();
+            string md = u.getMD5("aaa111...");
+
+            Assert.AreEqual(md, "15827d1b83c6be8de449fa4535737d48");
+
+            md = u.getMD5("");
+            Assert.IsNull(md);
+
+            md = u.getMD5(null);
+            Assert.IsNull(md);
+        }
+    }
+}
